afd5147e45a1f3f6af6a043889fef9821fe4e2cf
Soro08 <soro@nan.ci>
2021-02-24 00:17:17 +0000

    update code

--- marketwatch/settings.py
+++ marketwatch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '0@e%j-z#olhtt4d5z0e+*^d))a+cj=&$lrmqbs0)@3%e@fb%ms'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scrapword.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
